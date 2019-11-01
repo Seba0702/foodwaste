3dca0727bfbc78c9b55c50989e31bcedca519a42
Jakup <cavras2009@hotmail.com>
2019-11-01 13:08:47 +0100

    "Edited the character setup - with new instances"

--- src/foodwastes/Characters.java
+++ src/foodwastes/Characters.java
@@ -10,31 +10,49 @@ package foodwastes;
  * @author Cavra
  */
 public class Characters {
-    private int health;
-    private int hunger;
-    private String name;
+    public int health;
+    public int hunger;
+    public int loseHealth;
     
-    public Characters(int health, int hunger) {
-        this.health = health;
-        this.hunger = hunger;
-//        this.name = name;
-        
+    public Characters() {
+        health = 100;
+        hunger = 100;
+        loseHealth = 10;
     }
     
     public int getHealth() {
         return health;
     }
     
-/*    public void setplayerName(String name) {
-        this.name = name;
-    }*/
-    
-    public String getplayerName() {
-        return name;
-    }
-    
     public int getHunger() {
         return hunger;
     }
     
+    public void whenHungry() {
+        while(hunger < 50) {
+            System.out.println("You are hungry! Make sure to get something to eat");
+            
+        }
+    }
+    
+    public void subHealth() {
+        health = health - loseHealth;
+        while(1 > health) {
+            System.out.println("You died.");
+            break;
+        }
+        
+    }
+
+    public void subHunger() {
+        hunger = hunger - loseHealth;
+    }
+    
+/*    public void dead() {
+        while(0 > health) {
+            System.out.println("\tYou died.");
+            break;
+            
+        }
+    }*/
 }
